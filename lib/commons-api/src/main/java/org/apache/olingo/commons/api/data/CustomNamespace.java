9d64ff8bc2fbc7829706c6245b7b116c6cc87b34
Sam Gammon <sam@dyme.com>
2022-09-20 19:57:11 -0700

    Fix checkstyle violations

--- lib/commons-api/src/main/java/org/apache/olingo/commons/api/data/CustomNamespace.java
+++ lib/commons-api/src/main/java/org/apache/olingo/commons/api/data/CustomNamespace.java
@@ -60,8 +60,12 @@ public final class CustomNamespace {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         CustomNamespace that = (CustomNamespace) o;
         return Objects.equals(prefix, that.prefix) && Objects.equals(uri, that.uri);
     }
